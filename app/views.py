11ab65b73dd449aa2adf58082e55d179c6540fdc
Webis <akobyanrobert8@gmail.com>
2026-03-06 16:41:22 +0300

    Добавил недостающие функции обработчики для шаблонов шапки навигации.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,33 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def contacts(request):
+    return render(request, 'Contacts.html')
+
+def discounts(request):
+    return render(request, 'Discounts.html')
+
+def news(request):
+    return render(request, 'News.html')
+
+def online_registration(request):
+    return render(request, 'Online-registration.html')
+
+def ours_works(request):
+    return render(request, 'Ours-works.html')
+
+def personal_account(request):
+    return render(request, 'Personal-account.html')
+
+def prices(request):
+    return render(request, 'prices.html')
+
+def repair(request):
+    return render(request, 'prices.html')
+
+def reviews(request):
+    return render(request, 'Reviews.html')
+
+def shopping_cart(request):
+    return render(request, 'Shopping-cart.html')
