3ddab9eaac15a05d89fc8517828c6e8d3ba5ab64
Webis <akobyanrobert8@gmail.com>
2026-03-06 16:48:53 +0300

    Добавил недостающие маршруты шаблонов шапки навигации.

--- project/urls.py
+++ project/urls.py
@@ -23,4 +23,13 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index , name='Главная страница'),
     path('about/', views.about, name='О нас'),
+    path('contacts/', views.contacts, name='Контакты'),
+    path('discounts/', views.discounts, name='Скидки'),
+    path('news/', views.news, name='Новости'),
+    path('online-registration/', views.online_registration, name='Онлайн запись'),
+    path('ours-works/', views.ours_works, name='Наши работы'),
+    path('personal-account/', views.personal_account, name='Личный кабинет'),
+    path('prices/', views.prices, name='Цены'),
+    path('repair/', views.repair, name='Ремонт'),
+    path('reviews/', views.reviews, name='Отзывы'),
 ]
