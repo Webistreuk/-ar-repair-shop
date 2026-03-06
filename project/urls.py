faccc9264a0d38b1f431744dee14c2ff10822036
Webis <akobyanrobert8@gmail.com>
2026-03-06 16:15:15 +0300

    Создал функцию обработчик для страницы 'О нас', построил маршрут, в шаблоне главной страницы изменил тег p на тег a для определения ссылки, в стилях поменял p на тег якоря.

--- project/urls.py
+++ project/urls.py
@@ -21,5 +21,6 @@ from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index , name='Главная страница')
+    path('', views.index , name='Главная страница'),
+    path('about/', views.about, name='О нас'),
 ]
