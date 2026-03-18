20ef80d86c90498cd4bd244b734a3f440e0ee2de
Webis <akobyanrobert8@gmail.com>
2026-03-18 16:08:49 +0300

    добавлены все приложения

---
+++ Online_registration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OnlineRegistrationConfig(AppConfig):
+    name = "Online_registration"
---
+++ Ours_works/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OursWorksConfig(AppConfig):
+    name = "Ours_works"
